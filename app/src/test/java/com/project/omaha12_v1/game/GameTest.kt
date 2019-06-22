ecad5e4ba0f120ae8f56ab455aa09180478d7a9c
alonm <alonmor89@gmail.com>
2019-06-22 17:36:42 +0300

    add more tests for results

--- app/src/test/java/com/project/omaha12_v1/game/GameTest.kt
+++ app/src/test/java/com/project/omaha12_v1/game/GameTest.kt
@@ -72,7 +72,7 @@ class GameTest {
     }
 
     @Test
-    fun `calculate result for two player when there is a split koo`() {
+    fun `calculate result for two players when there is a split koo`() {
         val alonPlayer = PlayerImpl(
             "alonmo", listOf(),
             OmahaHand.fromString("KcKdAcAd")!!.cards,
@@ -100,4 +100,111 @@ class GameTest {
 
     }
 
+    @Test
+    fun `calculate result for three players when one win with complete`() {
+        val alonPlayer = PlayerImpl(
+            "alonmo", listOf(),
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("QcQdTcTd")!!.cards
+        )
+
+        val sagiPlayer = PlayerImpl(
+            "sagiel", listOf(),
+            OmahaHand.fromString("3s4s5s6s")!!.cards,
+            OmahaHand.fromString("3h4h5h6h")!!.cards,
+            OmahaHand.fromString("3d4d5d6d")!!.cards
+        )
+
+        val grojanPlayer = PlayerImpl(
+            "grojan", listOf(),
+            OmahaHand.fromString("3s4s5s6s")!!.cards,
+            OmahaHand.fromString("3h4h5h6h")!!.cards,
+            OmahaHand.fromString("3d4d5d6d")!!.cards
+        )
+
+        val boards = GameBoardImpl.fromString("TsJdQh2h2s", "TsJsQh2h2d", "TsJsQh2h2d")
+        val gameBoard = GameBoardImpl(boards[0], boards[1], boards[2])
+
+        val dealer = DealerImpl(CardDeckFactory().build(), ShowDownEvaluatorImpl())
+        val game = Omaha12Game(dealer, gameBoard, listOf(alonPlayer, sagiPlayer, grojanPlayer))
+
+        val gameResult = game.calculateResult()
+
+        assertThat(gameResult.playersResult.contains(PlayerResult("alonmo", 12.0)), equalTo(true))
+        assertThat(gameResult.playersResult.contains(PlayerResult("sagiel", 0.0)), equalTo(true))
+        assertThat(gameResult.playersResult.contains(PlayerResult("grojan", 0.0)), equalTo(true))
+    }
+
+    @Test
+    fun `calculate result for three players when all splits`() {
+        val alonPlayer = PlayerImpl(
+            "alonmo", listOf(),
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("QcQdTcTd")!!.cards
+        )
+
+        val sagiPlayer = PlayerImpl(
+            "sagiel", listOf(),
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("QcQdTcTd")!!.cards
+        )
+
+        val grojanPlayer = PlayerImpl(
+            "grojan", listOf(),
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("QcQdTcTd")!!.cards
+        )
+
+        val boards = GameBoardImpl.fromString("TsJdQh2h2s", "TsJsQh2h2d", "TsJsQh2h2d")
+        val gameBoard = GameBoardImpl(boards[0], boards[1], boards[2])
+
+        val dealer = DealerImpl(CardDeckFactory().build(), ShowDownEvaluatorImpl())
+        val game = Omaha12Game(dealer, gameBoard, listOf(alonPlayer, sagiPlayer, grojanPlayer))
+
+        val gameResult = game.calculateResult()
+
+        assertThat(gameResult.playersResult.contains(PlayerResult("alonmo", 1.0)), equalTo(true))
+        assertThat(gameResult.playersResult.contains(PlayerResult("sagiel", 1.0)), equalTo(true))
+        assertThat(gameResult.playersResult.contains(PlayerResult("grojan", 1.0)), equalTo(true))
+    }
+
+    @Test
+    fun `calculate result for three players were 2 - 1 - 0`() {
+        val alonPlayer = PlayerImpl(
+            "alonmo", listOf(),
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("KcKdAcAd")!!.cards,
+            OmahaHand.fromString("QcQdTcTd")!!.cards
+        )
+
+        val sagiPlayer = PlayerImpl(
+            "sagiel", listOf(),
+            OmahaHand.fromString("3c4c5c6c")!!.cards,
+            OmahaHand.fromString("3h4h5h6h")!!.cards,
+            OmahaHand.fromString("7d8c5d6d")!!.cards
+        )
+
+        val grojanPlayer = PlayerImpl(
+            "grojan", listOf(),
+            OmahaHand.fromString("3c4c5c6c")!!.cards,
+            OmahaHand.fromString("3h4h5h6h")!!.cards,
+            OmahaHand.fromString("3h4h5h6h")!!.cards
+        )
+
+        val boards = GameBoardImpl.fromString("TsJdQh2h2s", "TsJsQh2h2d", "Jc9c8s7c7h")
+        val gameBoard = GameBoardImpl(boards[0], boards[1], boards[2])
+
+        val dealer = DealerImpl(CardDeckFactory().build(), ShowDownEvaluatorImpl())
+        val game = Omaha12Game(dealer, gameBoard, listOf(alonPlayer, sagiPlayer, grojanPlayer))
+
+        val gameResult = game.calculateResult()
+
+        assertThat(gameResult.playersResult.contains(PlayerResult("alonmo", 2.0)), equalTo(true))
+        assertThat(gameResult.playersResult.contains(PlayerResult("sagiel", 1.0)), equalTo(true))
+        assertThat(gameResult.playersResult.contains(PlayerResult("grojan", 0.0)), equalTo(true))
+    }
 }
