8674149e1a1224e77e3fd2864cfab5f70cda25c7
sagi <sagi.elfassi@gmail.com>
2019-06-24 00:15:10 +0300

    all cards on board with image what alon failed to do

--- app/src/main/java/com/project/omaha12_v1/GameScreen.kt
+++ app/src/main/java/com/project/omaha12_v1/GameScreen.kt
@@ -1,6 +1,8 @@
 package com.project.omaha12_v1
 
 import android.content.pm.ActivityInfo
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -33,33 +35,32 @@ class GameScreen : AppCompatActivity() {
 
         game.startNewGame()
 
-        player1card1.text = players[0].cards()[0].toString()
-        player1card2.text = players[0].cards()[1].toString()
-        player1card3.text = players[0].cards()[2].toString()
-        player1card4.text = players[0].cards()[3].toString()
-        player1card5.text = players[0].cards()[4].toString()
-        player1card6.text = players[0].cards()[5].toString()
-        player1card7.text = players[0].cards()[6].toString()
-        player1card8.text = players[0].cards()[7].toString()
-        player1card9.text = players[0].cards()[8].toString()
-        player1card10.text = players[0].cards()[9].toString()
-        player1card11.text = players[0].cards()[10].toString()
-        player1card12.text = players[0].cards()[11].toString()
 
-        player2card1.text = players[1].cards()[0].toString()
-        player2card2.text = players[1].cards()[1].toString()
-        player2card3.text = players[1].cards()[2].toString()
-        player2card4.text = players[1].cards()[3].toString()
-        player2card5.text = players[1].cards()[4].toString()
-        player2card6.text = players[1].cards()[5].toString()
-        player2card7.text = players[1].cards()[6].toString()
-        player2card8.text = players[1].cards()[7].toString()
-        player2card9.text = players[1].cards()[8].toString()
-        player2card10.text = players[1].cards()[9].toString()
-        player2card11.text = players[1].cards()[10].toString()
-        player2card12.text = players[1].cards()[11].toString()
+        player1card1.setImageBitmap(getBitmapPath(players[0].cards()[0].toString()))
+        player1card2.setImageBitmap(getBitmapPath(players[0].cards()[1].toString()))
+        player1card3.setImageBitmap(getBitmapPath(players[0].cards()[2].toString()))
+        player1card4.setImageBitmap(getBitmapPath(players[0].cards()[3].toString()))
+        player1card5.setImageBitmap(getBitmapPath(players[0].cards()[4].toString()))
+        player1card6.setImageBitmap(getBitmapPath(players[0].cards()[5].toString()))
+        player1card7.setImageBitmap(getBitmapPath(players[0].cards()[6].toString()))
+        player1card8.setImageBitmap(getBitmapPath(players[0].cards()[7].toString()))
+        player1card9.setImageBitmap(getBitmapPath(players[0].cards()[8].toString()))
+        player1card10.setImageBitmap(getBitmapPath(players[0].cards()[9].toString()))
+        player1card11.setImageBitmap(getBitmapPath(players[0].cards()[10].toString()))
+        player1card12.setImageBitmap(getBitmapPath(players[0].cards()[11].toString()))
 
-        tempCard.setImageResource(R.drawable.ace_spade)
+        player2card1.setImageBitmap(getBitmapPath(players[1].cards()[0].toString()))
+        player2card2.setImageBitmap(getBitmapPath(players[1].cards()[1].toString()))
+        player2card3.setImageBitmap(getBitmapPath(players[1].cards()[2].toString()))
+        player2card4.setImageBitmap(getBitmapPath(players[1].cards()[3].toString()))
+        player2card5.setImageBitmap(getBitmapPath(players[1].cards()[4].toString()))
+        player2card6.setImageBitmap(getBitmapPath(players[1].cards()[5].toString()))
+        player2card7.setImageBitmap(getBitmapPath(players[1].cards()[6].toString()))
+        player2card8.setImageBitmap(getBitmapPath(players[1].cards()[7].toString()))
+        player2card9.setImageBitmap(getBitmapPath(players[1].cards()[8].toString()))
+        player2card10.setImageBitmap(getBitmapPath(players[1].cards()[9].toString()))
+        player2card11.setImageBitmap(getBitmapPath(players[1].cards()[10].toString()))
+        player2card12.setImageBitmap(getBitmapPath(players[1].cards()[11].toString()))
 
         game.open3Flops()
 
@@ -67,17 +68,21 @@ class GameScreen : AppCompatActivity() {
         val secondFlop = game.gameBoard.secondKoo()
         val thirdFlop = game.gameBoard.thirdKoo()
 
-        flop1card1.text = firstFlop[0].toString()
-        flop1card2.text = firstFlop[1].toString()
-        flop1card3.text = firstFlop[2].toString()
+        flop1card1.setImageBitmap(getBitmapPath(firstFlop[0].toString()))
+        flop1card2.setImageBitmap(getBitmapPath(firstFlop[1].toString()))
+        flop1card3.setImageBitmap(getBitmapPath(firstFlop[2].toString()))
 
-        flop2card1.text = secondFlop[0].toString()
-        flop2card2.text = secondFlop[1].toString()
-        flop2card3.text = secondFlop[2].toString()
+        flop2card1.setImageBitmap(getBitmapPath(secondFlop[0].toString()))
+        flop2card2.setImageBitmap(getBitmapPath(secondFlop[1].toString()))
+        flop2card3.setImageBitmap(getBitmapPath(secondFlop[2].toString()))
 
-        flop3card1.text = thirdFlop[0].toString()
-        flop3card2.text = thirdFlop[1].toString()
-        flop3card3.text = thirdFlop[2].toString()
+        flop3card1.setImageBitmap(getBitmapPath(thirdFlop[0].toString()))
+        flop3card2.setImageBitmap(getBitmapPath(thirdFlop[1].toString()))
+        flop3card3.setImageBitmap(getBitmapPath(thirdFlop[2].toString()))
 
+    }
+    fun getBitmapPath(bit: String ): Bitmap?{
+        val assetsBitmap: Bitmap? = BitmapFactory.decodeStream(assets.open(bit.toLowerCase()+".png"))
+        return assetsBitmap
     }
 }
